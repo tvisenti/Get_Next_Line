92ed74740efd5dcc0c88bf0cf172ab778c26f76e
Thomas VISENTIN <tvisenti@e2r8p9.42.fr>
2016-02-08 19:00:23 +0100

    GNL ok, pb with basic tests on 42fc

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/09 10:18:13 by tvisenti          #+#    #+#             */
-/*   Updated: 2016/02/04 17:03:48 by tvisenti         ###   ########.fr       */
+/*   Updated: 2016/02/08 18:55:49 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,23 @@ static char			**ft_allocate(char **save, int fd)
 	return (new_save);
 }
 
+static char			*ft_strrejoin(char *s1, char *s2, size_t len)
+{
+	char		*str;
+	int			nb;
+	char		*tmp;
+
+	nb = ft_strlen(s1) + ++len;
+	str = ft_strnew(nb);
+	tmp = str;
+	while (*s1)
+		*str++ = *s1++;
+	while (*s2 && --len > 0)
+		*str++ = *s2++;
+	*str = '\0';
+	return (str - (str - tmp));
+}
+
 static char			*ft_chrandcpy(char *save)
 {
 	if (ft_strchr(save, '\n'))
@@ -60,6 +77,7 @@ int					get_next_line(int const fd, char **line)
 	char		buff[BUFF_SIZE + 1];
 	static char	**save = NULL;
 	int			res;
+	char		*ptr;
 
 	if (fd < 0 || BUFF_SIZE < 1 || !line || read(fd, buff, 0) < 0)
 		return (-1);
@@ -69,31 +87,12 @@ int					get_next_line(int const fd, char **line)
 	(res = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[res] = '\0';
-		save[fd] = ft_strjoin(save[fd], buff);
+		ptr = save[fd];
+		save[fd] = ft_strrejoin(save[fd], buff, res);
+		free(ptr);
 	}
 	*line = ft_strsub(save[fd], 0, ft_strclen(save[fd]));
 	if (ft_chrandcpy(save[fd]) == NULL)
 		return (0);
 	return (1);
 }
-/*
-static char			*ft_chrandcpy(char *save)
-{
-	char	*new;
-
-	if (ft_strchr(save, '\n'))
-	{
-		new = malloc(sizeof(char *) * ft_strclen(save) + 1);
-		ft_strcpy(new, ft_strchr(save, '\n') + 1);
-		free(save);
-		return (new);
-	}
-	if (ft_strclen(save) > 0)
-	{
-		new = malloc(sizeof(char *) * ft_strclen(save));
-		ft_strcpy(new, ft_strchr(save, '\0'));
-		free(save);
-		return (new);
-	}
-	return (NULL);
-}*/
